237245302ab693ded25bd7aacdceff072e71afce
masrafi115 <53406617+masrafi115@users.noreply.github.com>
2025-03-10 17:46:29 +0600

    changed

--- components/Head.jsx
+++ components/Head.jsx
@@ -18,7 +18,7 @@ const CustomHead = ({ title }) => {
         content="A full-stack developer building websites that you'd like to use."
       />
       <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:url" content="https://masrafi115.github.io/vscode-portfolio" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
